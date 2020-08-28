b4cafefa152bccb4cd2c4bc62ef370a9f81fb4a1
Kraig Spear <kraigspear@gmail.com>
2020-08-28 05:07:43 -0400

    Initial Commit

---
+++ Submodule/Submodule.h
@@ -0,0 +1,19 @@
+//
+//  Submodule.h
+//  Submodule
+//
+//  Created by Kraig Spear on 8/28/20.
+//  Copyright © 2020 DICKSSportingGoods. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Submodule.
+FOUNDATION_EXPORT double SubmoduleVersionNumber;
+
+//! Project version string for Submodule.
+FOUNDATION_EXPORT const unsigned char SubmoduleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Submodule/PublicHeader.h>
+
+
